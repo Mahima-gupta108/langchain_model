2b14ca696d46f0390f588bade905b86f7342ff95
Mahima-gupta108 <guptamahima108@gmail.com>
2025-09-02 22:10:35 +0530

    Update prompt_ui.py

--- prompt_ui.py
+++ prompt_ui.py
@@ -3,17 +3,13 @@ from dotenv import load_dotenv
 import streamlit as st
 import os
 
-# Load .env locally (ignored on Render)
 load_dotenv()
-
-# Get HuggingFace API key from environment variable
 api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 if not api_key:
     st.error("API key not found! Set HUGGINGFACEHUB_API_TOKEN in .env or environment variables.")
     st.stop()
 
-# Initialize HuggingFace LLM with API key
 llm = HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-V3.1",
     task="text-generation",
@@ -22,7 +18,6 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm=llm)
 
-# Streamlit UI
 st.header("Research Tool")
 user_input = st.text_input("Enter your prompt here")
 
@@ -30,6 +25,7 @@ if st.button('Summarize') and user_input:
    
     result = model.invoke(user_input)
     st.write(result.content)
+
 
 
 
